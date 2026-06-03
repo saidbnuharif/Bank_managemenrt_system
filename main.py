7ddc8adc36446577eb6197a4c826e14af56a9e58
Saidbnu Harif C <saidbnuharif222@gmail.com>
2026-06-02 23:12:31 -0700

    add_Withdrawal

--- main.py
+++ main.py
@@ -38,7 +38,12 @@ class Bank:
         print("Amount deposited successfully")
 
     def withdraw(self, amount):
+        if self.balance < amount:
+            print("Insufficient balance")
+            return
         self.balance -= amount
+        self.history.append(f"₹{amount} Withdrawed at {current_time} on {current_date}")
+        print("Amount withdrawed succesfully")
 
     def add_history(self, message):
         self.history.append(message)
@@ -77,9 +82,9 @@ while True:
 
     print("""
     1.Create Account
-    2.Deposit
-    3.Show Balance
-    4.Withdraw
+    2.Deposit     
+    3.Withdraw
+    4.Show Balance
     5.Show all account
     6.Remove
     7.Transfer money
@@ -144,153 +149,27 @@ while True:
             print("No account found")
 
     elif choice == "3":
-        name = input("Enter account name: ").lower()
-        for account in data:
-            if account["name"] == name:
-                bank = Bank(account["name"], account["balance"])
-                bank.show_balance()
-                break
-
-    elif choice == "4":
-        name = input("Enter account holder name: ")
-        for account in data:
-            if account["name"] == name:
-                try:
-                    withdrawal_amount = int(input("Enter the amount to withdraw: "))
-                except:
-                    print("Amount must be numbers only")
-                    continue
-                Available_balance = account["balance"]
-                if Available_balance >= withdrawal_amount:
-                    bank = Bank(account["name"], account["balance"])
-                    bank.withdraw(withdrawal_amount)
-                    account["balance"] = bank.balance
-                    account["history"].append(f"Withdrawal ₹{withdrawal_amount}")
-                    save_data()
-                    print("Money withdrawed successfully")
-
-                elif Available_balance < withdrawal_amount:
-                    print("Low balance")
-
-    elif choice == "5":
-        for account in data:
-            if not data:
-                print("List is empty")
-            print(f"Name: {account["name"]} | Balance: {account["balance"]}")
-        print("Accounts fetched")
-
-    elif choice == "6":
-        name = input("Enter account name: ").lower()
-        for account in data:
-            if account["name"] == name:
-                bank = Bank(account["name"], account["balance"], account["history"])
-                data.remove(account)
-                save_data()
-                print(bank.remove_message())
-                break
-        else:
-            print("No account found")
-
-    elif choice == "7":
-        name = input("Enter account name: ")
-        for account in data:
-            if account["name"] == name:
-                balance = account["balance"]
-                receiver_name = input("Enter receiver account name: ")
-
-                for item in data:
-                    if item["name"] == receiver_name:
-                        receiver_balance = item["balance"]
-                        try:
-                            transferring_amount = int(
-                                input("Enter money to transfer: ")
-                            )
-                        except:
-                            print("Amount must be numbers only")
-                            continue
-                        if balance >= transferring_amount:
-                            item["balance"] = receiver_balance + transferring_amount
-                            account["balance"] = balance - transferring_amount
-
-                            account["history"].append(
-                                f"Sent ₹{transferring_amount} to {receiver_name}"
-                            )
-                            item["history"].append(
-                                f"Received ₹{transferring_amount} from {name}"
-                            )
-
-                            save_data()
-                            print("Amount transferred successfully")
-
-                        else:
-                            print("Low balance")
-                        break
-
-                else:
-                    print("reciver  not found")
-                break
-
-        else:
-            print("Sender not found")
-
-    elif choice == "8":
-        name = input("Enter account name: ")
+        try:
+            account_no = int(input("Enter account number: "))
+        except ValueError:
+            print("Account number must be numbers only")
+            continue
         for accounts in data:
-            if accounts["name"] == name:
-                print(f"Transactions: {accounts['history']}")
-                break
-        else:
-            print("No account found")
-
-    elif choice == "9":
-        name = input("Enter account name: ").lower()
-
-        for account in data:
-            if account["name"] == name:
-                for transaction in account["history"]:
-                    if "Sent" in transaction:
-                        print(transaction)
-                break
-        else:
-            print("Account not found")
-
-    elif choice == "10":
-        name = input("Enter account name: ")
-        for account in data:
-            if account["name"] == name:
-                for transaction in account["history"]:
-                    if "Received" in transaction:
-                        print(transaction)
-                    else:
-                        print("No money recieved")
-            break
-        else:
-            print("No user found")
-
-    elif choice == "11":
-        name = input("Enter account holder name: ")
-        for account in data:
-            if account["name"] == name:
-                for details in account["history"]:
-                    if "Deposit" in details:
-                        print(details)
-
-                    else:
-                        print("No history found")
+            if accounts["Account_number"] == account_no:
+                try:
+                    amount = float(input("Enter amount to transfer: "))
+                except ValueError:
+                    print("Amount must be numbers only")
                     break
-            break
-        else:
-            print("No user found")
-
-    elif choice == "12":
-        if not data:
-            print("No accounts found")
-        else:
-            richest_account = data[0]
-            for account in data:
-                if account["balance"] > richest_account["balance"]:
-                    richest_account = account
-        print(f"Richest Account: {richest_account}")
+                account = Bank(
+                    accounts["Account_number"],
+                    accounts["name"],
+                    accounts["balance"],
+                    accounts["history"],
+                )
+                account.withdraw(amount)
+                accounts["balance"] = account.balance
+                accounts["history"] = account.history
 
     elif choice == "13":
         break
